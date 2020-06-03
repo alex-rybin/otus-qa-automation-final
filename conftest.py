c0dc4bdd0c3cf12da2cddba94c14264498486eb0
Alexander <alex_rybin@hotmail.com>
2020-06-03 17:58:44 +0300

    Add basic configs

---
+++ conftest.py
@@ -0,0 +1,123 @@
+import logging
+
+import pytest
+from envparse import env
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.abstract_event_listener import AbstractEventListener
+from selenium.webdriver.support.event_firing_webdriver import EventFiringWebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+env.read_envfile('.env.local')
+BASE_URL = f'http://{env.str("PRESTASHOP_HOST")}'
+BROWSER_CHOICES = ['firefox', 'chrome', 'opera']
+HEADER = (By.XPATH, '//nav[@id="top"]')  # todo: поправить
+
+
+class EventListener(AbstractEventListener):
+    def __init__(self, logger):
+        super().__init__()
+        self.logger = logger
+
+    def after_change_value_of(self, element, driver):
+        self.logger.info(f'Changed value of {element}')
+
+    def after_click(self, element, driver):
+        self.logger.info(f'Clicked on {element}')
+
+    def after_close(self, driver):
+        self.logger.info(f'Closed window of {driver}')
+
+    def after_execute_script(self, script, driver):
+        self.logger.info(f'Executed script: {script}')
+
+    def after_find(self, by, value, driver):
+        self.logger.info(f'Searched {value} by {by}')
+
+    def after_navigate_back(self, driver):
+        self.logger.info('Navigated back')
+
+    def after_navigate_forward(self, driver):
+        self.logger.info('Navigated forward')
+
+    def after_navigate_to(self, url, driver):
+        self.logger.info(f'Opened URL: {url}')
+
+    def after_quit(self, driver):
+        self.logger.info('Browser quit')
+
+    def before_change_value_of(self, element, driver):
+        self.logger.info(f'Changing value of {element}')
+
+    def before_click(self, element, driver):
+        self.logger.info(f'Clicking on {element}')
+
+    def before_close(self, driver):
+        self.logger.info('Closing window')
+
+    def before_execute_script(self, script, driver):
+        self.logger.info(f'Executing script: {script}')
+
+    def before_find(self, by, value, driver):
+        self.logger.info(f'Searching {value} by {by}')
+
+    def before_navigate_back(self, driver):
+        self.logger.info('Navigating back')
+
+    def before_navigate_forward(self, driver):
+        self.logger.info('Navigating forward')
+
+    def before_navigate_to(self, url, driver):
+        self.logger.info(f'Opening URL: {url}')
+
+    def before_quit(self, driver):
+        self.logger.info('Quitting browser')
+
+    def on_exception(self, exception, driver):
+        self.logger.warning(f'Exception thrown: {exception}')
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '-B',
+        '--browser',
+        default='firefox',
+        choices=BROWSER_CHOICES,
+        help='Browser to use in tests. Can be firefox, chrome or opera. Default: firefox',
+    )
+
+
+@pytest.fixture(scope='session')
+def logger():
+    logging.basicConfig(
+        format='%(asctime)s %(name)s [%(levelname)s]: %(message)s',
+        level=logging.INFO,
+        force=True,
+    )
+    return logging.getLogger('Fixture')
+
+
+@pytest.fixture
+def browser(logger, request):
+    selected_browser = request.config.getoption('--browser')
+
+    browser = EventFiringWebDriver(
+        webdriver.Remote(
+            command_executor=f'http://{env.str("SELENOID_HOST")}:4444/wd/hub',
+            desired_capabilities={
+                'browserName': selected_browser,
+                'loggingPrefs': {'browser': 'ALL'},
+                'acceptInsecureCerts': True,
+            },
+        ),
+        EventListener(logging.getLogger('Browser')),
+    )
+
+    request.addfinalizer(browser.quit)
+
+    browser.get(f'http://{env.str("PRESTASHOP_HOST")}')
+    # Ожидание на случай первоначальной установки
+    WebDriverWait(browser, 100).until(EC.visibility_of_element_located(HEADER))
+
+    return browser
