edf350f33f61a65ddeb749ee4c43e4a894f6d10b
Alexander Rybin <alex_rybin@hotmail.com>
2020-06-17 22:11:21 +0300

    Add main page test

--- sources/pages/customer/main.py
+++ sources/pages/customer/main.py
@@ -1,9 +1,26 @@
+from selenium.webdriver.remote import webelement
+
+from sources.logic.common import Locator
 from sources.pages.customer.base import StoreBasePage, StoreBasePageLocators
 
 
 class StoreMainPageLocators(StoreBasePageLocators):
-    pass
+    CAROUSEL = Locator('//div[@id="carousel"]')
+    FEATURED = Locator('//div[@class="products"]')
 
 
 class StoreMainPage(StoreBasePage):
-    pass
+    _carousel = None
+    _featured = None
+
+    @property
+    def carousel(self) -> webelement:
+        if not self._carousel:
+            self._carousel = self.browser.find_element(*StoreMainPageLocators.CAROUSEL)
+        return self._carousel
+
+    @property
+    def featured(self) -> webelement:
+        if not self._featured:
+            self._featured = self.browser.find_element(*StoreMainPageLocators.FEATURED)
+        return self._featured
